f0331337012e5e7c32d4fa2a6e549d55c52a484a
Kanabay, Kirill <kkanabay@qulix.com>
2022-06-16 19:56:23 +0300

    Added converter

---
+++ src/MedicationManager.UI.Common/Converters/BooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MedicationManager.UI.Common.Converters
+{
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? "+" : "-";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = (string) value;
+
+            return str.Equals("+");
+        }
+    }
+}
